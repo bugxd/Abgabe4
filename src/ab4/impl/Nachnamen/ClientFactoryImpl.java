96d7beccf148721aa47ddefb8698a606607a3364
Lukas Feistritzer <luki.feise@gmail.com>
2016-05-17 11:54:52 +0200

    Create Project

---
+++ src/ab4/impl/Nachnamen/ClientFactoryImpl.java
@@ -0,0 +1,35 @@
+package ab4.impl.Nachnamen;
+
+import ab4.CRClientA;
+import ab4.CRClientB;
+import ab4.ClientFactory;
+import ab4.DHClientA;
+import ab4.DHClientB;
+
+public class ClientFactoryImpl implements ClientFactory {
+
+	@Override
+	public CRClientA generateCRClientA() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CRClientB generateCRClientB() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DHClientA generateDHClientA() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DHClientB generateDHClientB() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
