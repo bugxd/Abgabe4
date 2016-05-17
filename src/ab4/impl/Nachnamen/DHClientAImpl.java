b057ac9371fde0338b4e44afe02b20cb296bb398
Lukas Feistritzer <luki.feise@gmail.com>
2016-05-17 12:52:57 +0200

    Add Constructors

--- src/ab4/impl/Nachnamen/DHClientAImpl.java
+++ src/ab4/impl/Nachnamen/DHClientAImpl.java
@@ -15,6 +15,11 @@ public class DHClientAImpl implements ab4.DHClientA {
     private BigInteger Generator;
     private BigInteger SecretValue;
     private BigInteger PartOfSecret;
+    
+    public DHClientAImpl(){
+        super();
+        this.SecretValue = generatePrime(Prime.bitLength() - 1);
+    }
 
     @Override
     public void setBitLength(int n) {
@@ -36,7 +41,7 @@ public class DHClientAImpl implements ab4.DHClientA {
     //A = g^a b mod p
     @Override
     public BigInteger sendPartOfSecret() {
-        SecretValue = generatePrime(Prime.bitLength() - 1);
+
         return Generator.modPow(SecretValue,Prime);
     }
 
