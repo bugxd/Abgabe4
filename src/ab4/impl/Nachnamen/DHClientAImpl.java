9b5a10c04007231a6747c85bcf17f795440a7197
Lukas Feistritzer <luki.feise@gmail.com>
2016-05-17 12:46:15 +0200

    Finish

--- src/ab4/impl/Nachnamen/DHClientAImpl.java
+++ src/ab4/impl/Nachnamen/DHClientAImpl.java
@@ -3,41 +3,99 @@ package ab4.impl.Nachnamen;
 import ab4.DHClientA;
 
 import java.math.BigInteger;
+import java.security.SecureRandom;
 
 /**
  * Created by Luki on 17.05.2016.
  */
 public class DHClientAImpl implements ab4.DHClientA {
-    private int bitLength;
-    private BigInteger secret;
+    private int BitLength;
+    private BigInteger Secret;
+    private BigInteger Prime;
+    private BigInteger Generator;
+    private BigInteger SecretValue;
+    private BigInteger PartOfSecret;
 
     @Override
     public void setBitLength(int n) {
-
+        this.BitLength = n;
     }
 
     @Override
     public BigInteger sendPrime() {
-        return null;
+        this.Prime = generatePrime(this.BitLength);
+        return this.Prime;
     }
 
     @Override
     public BigInteger sendGenerator() {
-        return null;
+        this.Generator = generateGenerator(Prime);
+        return this.Generator;
     }
 
+    //A = g^a b mod p
     @Override
     public BigInteger sendPartOfSecret() {
-        return null;
+        SecretValue = generatePrime(Prime.bitLength() - 1);
+        return Generator.modPow(SecretValue,Prime);
     }
 
+    //K_1 = B^a mod p
     @Override
     public void receivePartOfSecret(BigInteger b) {
+        this.PartOfSecret = b;
 
+        this.Secret = PartOfSecret.modPow(SecretValue,Prime);
     }
 
     @Override
     public BigInteger getSecret() {
-        return null;
+        return this.Secret;
+    }
+
+    public BigInteger generatePrime(int n) {
+
+        SecureRandom rnd = new SecureRandom();
+        BigInteger q;
+        BigInteger p;
+
+        while (true)
+        {
+            // generate prime number q with given bit length
+            q = BigInteger.probablePrime(n, rnd);
+            // calculate p = 2q + 1
+            p = q.add(q).add(BigInteger.ONE);
+
+            // check, if p is prime
+            if (p.isProbablePrime(100)) // if p is prime, then ...
+                return p;				// return p
+        }
+    }
+
+    public BigInteger generateGenerator(BigInteger p) {
+        SecureRandom r = new SecureRandom();
+
+        BigInteger a;
+        BigInteger two = BigInteger.ONE.add(BigInteger.ONE);
+
+        while (true)
+        {
+            // create a (random) prime with same bitlength as p
+            a = new BigInteger(p.bitLength(), r);
+
+            // a has to be smaller than p
+            if (a.compareTo(p) < 0)
+            {
+                // a^2 mod p must not be 1, because then it would be either 1 or p-1
+                if (a.modPow(two, p).compareTo(BigInteger.ONE) != 0)
+                {
+                    // if a^p mod p is not 1 ...
+                    if (a.modPow(p, p).compareTo(BigInteger.ONE) != 0)
+                    {
+                        return a; // then return a
+                    }
+                }
+            }
+        }
     }
 }
--- src/ab4/impl/Nachnamen/DHClientBImpl.java
+++ src/ab4/impl/Nachnamen/DHClientBImpl.java
@@ -1,12 +1,17 @@
 package ab4.impl.Nachnamen;
 
 import java.math.BigInteger;
+import java.security.SecureRandom;
 
 /**
  * Created by Luki on 17.05.2016.
  */
 public class DHClientBImpl implements ab4.DHClientB{
-    private BigInteger secret;
+    private BigInteger Secret;
+    private BigInteger Prime;
+    private BigInteger Generator;
+    private BigInteger SecretValue;
+    private BigInteger PartOfSecret;
 
     public DHClientBImpl() {
         super();
@@ -14,26 +19,52 @@ public class DHClientBImpl implements ab4.DHClientB{
 
     @Override
     public void receivePrime(BigInteger p) {
-
+        this.Prime = p;
     }
 
     @Override
     public void receiveGenerator(BigInteger q) {
-
+        this.Generator = q;
     }
 
+    //K_2 = A^b mod p
     @Override
     public void receivePartOfSecret(BigInteger a) {
+        this.PartOfSecret = a;
 
+        SecretValue = generatePrime(Prime.bitLength() - 1);
+
+        this.Secret = PartOfSecret.modPow(SecretValue,Prime);
     }
 
+    //B = g^b mod p
     @Override
     public BigInteger sendPartOfSecret() {
-        return null;
+
+        return Generator.modPow(SecretValue,Prime);
     }
 
     @Override
     public BigInteger getSecret() {
-        return null;
+        return this.Secret;
+    }
+
+    public BigInteger generatePrime(int n) {
+
+        SecureRandom rnd = new SecureRandom();
+        BigInteger q;
+        BigInteger p;
+
+        while (true)
+        {
+            // generate prime number q with given bit length
+            q = BigInteger.probablePrime(n, rnd);
+            // calculate p = 2q + 1
+            p = q.add(q).add(BigInteger.ONE);
+
+            // check, if p is prime
+            if (p.isProbablePrime(100)) // if p is prime, then ...
+                return p;				// return p
+        }
     }
 }
